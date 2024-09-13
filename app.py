d2cb257d5c834211fd71d66fc9d0abdfcf311533
AquaticDrago <125418341+AquaticDrago@users.noreply.github.com>
2024-09-12 20:13:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+import pusher
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -15,18 +17,18 @@ def alumnos():
 
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
-    matricula = request.form["txtMatriculaFA"]
+    matricula      = request.form["txtMatriculaFA"]
     nombreapellido = request.form["txtNombreApellidoFA"]
-    return f"Matricula: {matricula} Nombre y Apellido: {nombreapellido}"
+    return f"Matrícula: {matricula} Nombre y Apellido: {nombreapellido}"
 
 @app.route("/evento")
 def evento():
     pusher_client = pusher.Pusher(
-        app_id='1714541',
-        key='cda1cc599395d699a2af',
-        secret='9e9c00fc36600060d9e2',
-        cluster='us2',
+        app_id="1714541",
+        key="cda1cc599395d699a2af",
+        secret="9e9c00fc36600060d9e2",
+        cluster="us2",
         ssl=True
     )
     
-    pusher_client.trigger('conexion', 'evento', {'message': 'hello world'})
+    pusher_client.trigger("conexion", "evento", {"txtTemperatura": 35, "txtHumedad": 0.6, "dpFechaHora": "2024-09-12 20:13:00"})
